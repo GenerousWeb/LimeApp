aa7b449cc9981ca4e1a8587491088cfe64c8c4a1
rajath.shanbhag <rajath.shanbhag@tafensw.edu.au>
2025-06-21 16:24:13 +1000

    feat: add basic auth

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,15 @@
 import Map from 'components/Map';
-import { Stack } from 'expo-router';
+import { Redirect, Stack } from 'expo-router';
 
 import SelectedBikeSheet from '~/components/SelectedBikeSheet';
 
 export default function Home() {
-  return (
-    <>
-      <Stack.Screen options={{ title: 'Home', headerShown: false }} />
-      <Map />
-      <SelectedBikeSheet />
-    </>
-  );
+  // return (
+  //   <>
+  //     <Stack.Screen options={{ title: 'Home', headerShown: false }} />
+  //     <Map />
+  //     <SelectedBikeSheet />
+  //   </>
+  // );
+  return <Redirect href="/auth" />;
 }
